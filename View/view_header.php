49a1b4a2319dc23604df72ad8dfcfb0811ffc7e8
Ram Lacia <RamLacia@gmail.com>
2012-09-26 22:33:43 +0800

    Beta 0

---
+++ View/view_header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html >
+<html lang="en">
+<head>
+<title>http://itsource.sg</title>
+<link rel="stylesheet" href="View/main.css" type="text/css" media="screen" />
+<link rel="shortcut icon" href="View/favicon.ico" type="image/x-icon"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("View/view_header.php"); ?>
+
+<?php include("View/view_body.php"); ?>
+
+<?php include("View/view_footer.php"); ?>
